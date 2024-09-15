4d58653f649944e2f468a44985a52c378f01ee03
code-uday <165893584+code-uday@users.noreply.github.com>
2024-09-15 21:30:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="udaypyadav628@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+//accountId=2
+accountEmail="hch@hc.com"
+accountPassword="22113"
+accountCity="benglore"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+/*
+Always use let....never use var...because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"//treat all js codde as newer version code
+//alert(3+3)//we are using node js here, not browser jisme engine alag ho document alag ho...yaha sb ek hi h
+console.log(3+2)
+console.log("Raju") //code readability should be high
+
+let name="Uday"//string 
+let age=21//number
+let isLoggedIn=false;//bool
+
+//number=> 2^53
+//bigint
+//string=>" "
+//boolean=>true/false
+//null=>stand alone value hai
+//undefined=> value nahi assign hui ho
+//symbol=>unique ke liye
+
+//object
+
+console.log(typeof null)//null ka type hota hai"object"
+console.log(typeof undefined)//undefined ka type hota hai "undefined"
