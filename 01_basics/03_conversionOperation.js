c657928e5fd2cc4ee72e8dd597b2a7b5734c199f
code-uday <165893584+code-uday@users.noreply.github.com>
2024-09-15 21:51:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="33abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let ValueInNumbber=Number(score)
+//console.log(typeof(ValueInNumbber))
+//console.log((ValueInNumbber))//gives NaN means not a number...mtlb number ko convert krne pe pura rely nhi krskte
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 false=0
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true ;0=>false
+//""=>false
+//"uday"=>true
+
+let someNum=33
+let stringnum=String(someNum)
+console.log(stringnum)
+console.log(typeof stringnum)
+//conversion ho sakta hai or tarah se ...jese abhi number ko string m convert kia
